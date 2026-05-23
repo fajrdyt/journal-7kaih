44ae039bd26838be6905e8485cb99301a17c1324
Fajar Hidayat <whdr952@gmail.com>
2026-05-23 16:26:29 +0700

    mock: initialisasi data mock untuk users, checkins, dan validations

---
+++ frontend/src/mocks/data/users.js
@@ -0,0 +1,13 @@
+import { ROLES } from '../constants/roles'
+
+export const users = [
+
+  {
+    id: 1,
+    name: 'Fajar',
+    email: 'admin@test.com',
+    password: 'password',
+    role: ROLES.STUDENT,
+  },
+
+]
