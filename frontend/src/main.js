b6078c5f69694e6514a976fe4581590ff1874b37
Fajar Hidayat <whdr952@gmail.com>
2026-06-01 20:59:34 +0700

    feat: add main.css import to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/main.css'
+
 async function enableMocking() {
 
   if (import.meta.env.DEV) {
