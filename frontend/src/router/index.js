b25d046d83942a68356b86693939faec8908a5cd
Fajar Hidayat <whdr952@gmail.com>
2026-06-09 12:45:50 +0700

    feat: Setup route student

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LoginPage from '../pages/auth/LoginPage.vue'
-import DashboardPage from '../pages/student/StudentDashboard.vue'
+import DashboardPage from '../pages/student/StudentDashboardPage.vue/index.js'
 
 import { useAuthStore } from '../stores/authStore'
 
@@ -26,6 +26,38 @@ const routes = [
       requiresAuth: true,
     },
   },
+
+  {
+  path: '/student',
+  meta: {
+    requiresAuth: true,
+  },
+  children: [
+    {
+      path: 'dashboard',
+      component: () =>
+        import('@/pages/student/StudentDashboardPage.vue'),
+    },
+
+    {
+      path: 'checkin',
+      component: () =>
+        import('@/pages/student/DailyCheckinPage.vue'),
+    },
+
+    {
+      path: 'history',
+      component: () =>
+        import('@/pages/student/CheckinHistoryPage.vue'),
+    },
+
+    {
+      path: 'recap',
+      component: () =>
+        import('@/pages/student/PersonalRecapPage.vue'),
+    },
+  ],
+}
   
 ]
 
