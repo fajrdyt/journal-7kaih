7ba7ef4030b782e33aba7bf618ad3537496a014d
Fajar Hidayat <whdr952@gmail.com>
2026-06-12 14:55:19 +0700

    feat (layout):buat layout utama dashboard student dengan sidebar dan header

---
+++ frontend/src/layouts/DashboardLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="min-h-screen bg-slate-50">
+    <Sidebar />
+
+    <div class="lg:ml-64">
+      <HeaderBar />
+
+      <main class="p-4 lg:p-6">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from '@/components/navigation/Sidebar.vue'
+import HeaderBar from '@/components/navigation/HeaderBar.vue'
+</script>
