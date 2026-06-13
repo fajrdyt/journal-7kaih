1c9701823d62be671634b7bfceddfdc5489d7985
Fajar Hidayat <whdr952@gmail.com>
2026-06-13 13:34:58 +0700

    refactor (mocks): ubah struktur data HABIT constant dari string menjadi objek array dilengkapi kode

--- frontend/src/mocks/constants/habits.js
+++ frontend/src/mocks/constants/habits.js
@@ -1,9 +1,45 @@
 export const HABITS = [
-  'Bangun Pagi',
-  'Beribadah',
-  'Berolahraga',
-  'Makan Sehat',
-  'Gemar Belajar',
-  'Bermasyarakat',
-  'Tidur Cepat',
+
+  {
+    id: 1,
+    code: 'wake_up_early',
+    name: 'Bangun Pagi',
+  },
+
+  {
+    id: 2,
+    code: 'worship',
+    name: 'Beribadah',
+  },
+
+  {
+    id: 3,
+    code: 'exercise',
+    name: 'Berolahraga',
+  },
+
+  {
+    id: 4,
+    code: 'healthy_eating',
+    name: 'Makan Sehat dan Bergizi',
+  },
+
+  {
+    id: 5,
+    code: 'love_learning',
+    name: 'Gemar Belajar',
+  },
+
+  {
+    id: 6,
+    code: 'community',
+    name: 'Bermasyarakat',
+  },
+
+  {
+    id: 7,
+    code: 'sleep_early',
+    name: 'Tidur Cepat',
+  },
+
 ]
